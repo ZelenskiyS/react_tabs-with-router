b8c8e93f93e8b071e1ab5ed379a24ee3dbf301aa
Simon <semen.zelenskiy@gmail.com>
2019-11-13 09:42:15 +0200

    Updates

---
+++ precache-manifest.a6f40796f03f137db5de0285d13d9189.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b81411d231c7dfbbfea13cdb4ce98d0",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "410bb1b64d9d5cea983a",
+    "url": "/react_tabs-with-router/static/css/main.5efc4f9c.chunk.css"
+  },
+  {
+    "revision": "6c3d4d606d1adbfad8db",
+    "url": "/react_tabs-with-router/static/js/2.1eb36e93.chunk.js"
+  },
+  {
+    "revision": "410bb1b64d9d5cea983a",
+    "url": "/react_tabs-with-router/static/js/main.db9f46a5.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
